3b5e6ebe03179abd7b1ed4699d5029d2381178be
irapanch <117355580+irapanch@users.noreply.github.com>
2024-07-22 17:34:08 +0300

    add weather point

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 require("dotenv").config();
 const TelegramApi = require("node-telegram-bot-api");
 const { gameOptions, againOptions } = require("./options");
+const axios = require("axios");
 
 const { TELEGRAM_BOT_TOKEN } = process.env;
 const bot = new TelegramApi(TELEGRAM_BOT_TOKEN, { polling: true });
 const chats = {};
+// const enterCity = async (chatId) => {
+//   await bot.sendMessage(
+//     chatId,
 
+//     "Enter your city"
+//   );
+// };
 const startGame = async (chatId) => {
   await bot.sendMessage(
     chatId,
@@ -22,6 +29,7 @@ const start = () => {
     { command: "/start", description: "Greeting" },
     { command: "/info", description: "User`s info" },
     { command: "/game", description: "Гра вгадай число" },
+    { command: "/weather", description: "The weather in your place" },
   ]);
 
   bot.on("message", async (msg) => {
@@ -37,9 +45,36 @@ const start = () => {
         "Welcome to Godzilla-bot by author https://www.youtube.com/@VovanGodzilla "
       );
     }
+    if (text === "/weather") {
+      bot.on("message", async (msg) => {
+        const chatId = msg.chat.id;
+        const userInput = msg.text;
+
+        try {
+          const response = await axios.get(
+            `https://api.openweathermap.org/data/2.5/weather?q=${userInput}&appid=ea05f0b6617d998492f421c4335d3bba`
+          );
+          const data = response.data;
+          const weather = data.weather[0].description;
+          const temperature = data.main.temp - 273.15;
+          const city = data.name;
+          const humidity = data.main.humidity;
+          const pressure = data.main.pressure;
+          const windSpeed = data.wind.speed;
+          const message = `The weather in ${city} is ${weather} with a temperature of ${temperature.toFixed(
+            2
+          )}°C. The humidity is ${humidity}%, the pressure is ${pressure}hPa, and the wind speed is ${windSpeed}m/s.`;
+
+          bot.sendMessage(chatId, message);
+        } catch (error) {
+          bot.sendMessage(chatId, "City doesn't exist.");
+        }
+      });
+    }
     if (text === "/info") {
       return bot.sendMessage(chatId, `Your name is ${msg.from.first_name}`);
     }
+
     if (text === "/game") {
       return startGame(chatId);
     }
